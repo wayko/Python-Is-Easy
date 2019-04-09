2295cefb9b8d2d4e4c039aef00f03b8c48e76146
Wayko <programmers@dreamcpu.com>
2019-04-09 16:21:09 -0400

    Pirple Python Course Homework
    Pirple Python Course Homework # 1

---
+++ main.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 14:29:28 2019
+
+@author: jortiz
+@assignment: Program to list favorite artist information
+"""
+
+# Group Name 
+Group = "Gorillaz"
+
+# Animated Members of group
+AnimatedBandMembers = "Mudoc Niccals, 2-D, Noodle, Russel Hobbs"
+
+# Real life members of group
+RealLifeMembers = "Damon Albarn, Jamie Hewlett, Mike Smith, Karl Bossche, Jeff Wootton, Remi Kabaka, James Ford"
+
+
+# Genre of music
+Genre = "Alternate rock"
+
+# My favorite song
+FavoriteSong = "Feel Good Inc."
+
+# Duration of favorite song
+Duration = 413
+
+# Album where my favorite song can be found
+Album = "Demon Days"
+
+# Year song came out
+Year = "2005"
+ 
+# Print out all information.
+print("Group Name: " + Group)
+print("Animated Members: " + AnimatedBandMembers)
+print("Real Life Member: " + RealLifeMembers)
+print("Genre: " + Genre)
+print("Favorite Song: " + FavoriteSong)
+print("Duration of Song : " + str(Duration))
+print("Album Name: " + Album)
+print("Year of Album Release: " + Year)
---
+++ variable.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 09:19:03 2019
+
+@author: jortiz
+"""
+"""
+Global Variables
+"""
+#PI = 3.14
+#print(PI)
+#
+#one, two, three = 1,2,3
+#
+#
+#print(one)
+#print(two)
+#print(three)
+#two = 4
+#print(two)
+#print(one)
+#
+#Decimal = 1.1
+#print(Decimal)
+#
+#StringVar = "This is a string"  + "1"
+#print(StringVar)
+#
+#def FunctionName():
+#    print(one)
+#    newVar = "World"
+#    print(newVar)
+#    return 
+#FunctionName()
+##print(newVar)
+#
+#Five = 5
+#print(Five)
+
+count = 0
+print(count)
+count += 1
+print(count)
+
+mou
