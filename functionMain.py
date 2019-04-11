4743f5fa04688e8be9eb6b873fb02c319ff09afe
Wayko <programmers@dreamcpu.com>
2019-04-11 13:57:24 -0400

    Created Python Homework #2 Functions

---
+++ functionMain.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 10 16:17:28 2019
+
+@author: jortiz
+@Homework Assignment: #2 Functions
+"""
+
+def Genre():
+    strGenre = "Alternate rock"
+    print(strGenre)
+
+def Album():
+    strAlbum = "Demon Days"
+    print(strAlbum)
+
+def Year():
+    strYear = "2005"
+    print(strYear)
+
+def Duration():
+    intDuration = 413
+    print(intDuration)
+    
+def booleanExtra(guess):
+    isFound = False
+    number = 35
+    
+    if guess == number :
+        isFound = True
+        print(isFound)
+    else:
+         print(isFound)
+     
+
+    
+
+booleanExtra(37)
