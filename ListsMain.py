b52df00285a475bdd1d6cf36ac113da7a51b686e
Wayko <programmers@dreamcpu.com>
2019-04-18 15:05:01 -0400

    Working on Lists Python

---
+++ ListsMain.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 15 13:08:16 2019
+
+@author: jortiz
+"""
+
+Scores = [70,85,67.5,90,80]
+
+print(Scores)
+print(Scores[0:3])
+Scores[0] = 80
+print(Scores)
