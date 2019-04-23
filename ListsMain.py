298800fe8144932c53b594eef409d28d684824e2
Wayko <programmers@dreamcpu.com>
2019-04-23 15:24:24 -0400

    Creating array list that does not allow duplicates

--- ListsMain.py
+++ ListsMain.py
@@ -5,9 +5,27 @@ Created on Mon Apr 15 13:08:16 2019
 @author: jortiz
 """
 
-Scores = [70,85,67.5,90,80]
+myUniqueList = []
+myLeftOvers = []
+ 
 
-print(Scores)
-print(Scores[0:3])
-Scores[0] = 80
-print(Scores)
+def addToList(addItem):
+    seen = set(myUniqueList)
+    if addItem not in seen:
+        seen.add(addItem)
+        myUniqueList.append(addItem)
+    else:
+        print(str)
+        rejectedItem(addItem)
+       
+def rejectedItem(rejectedItem):
+    myLeftOvers.append(rejectedItem)
+    
+addToList(1)
+addToList(2)
+addToList(3)
+addToList(3)
+addToList(4)
+addToList(2)
+print(myUniqueList)
+print(myLeftOvers)
